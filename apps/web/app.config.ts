a4d34a3c8313a7aefdca9a392438a3cc9ba0d152
rgarcia <rgarcia2009@gmail.com>
2025-02-11 12:59:48 -0500

    patch clerk to work with bun

--- apps/web/app.config.ts
+++ apps/web/app.config.ts
@@ -5,7 +5,11 @@ export default defineConfig({
   vite: {
     // node_modules/vinxi/dist/types/lib/vite-dev.d.ts needs to be updated to comment out the omittion of 'server' for this to work
     server: {
-      allowedHosts: ["raf--cannon.ngrok.app"],
+      allowedHosts: [
+        "raf--cannon.ngrok.app",
+        "dev--cannon-web.raf.xyz",
+        "cannon-web.raf.xyz",
+      ],
     },
     plugins: [
       tsConfigPaths({
@@ -13,5 +17,7 @@ export default defineConfig({
       }),
     ],
   },
-  server: {},
+  server: {
+    preset: "bun",
+  },
 });
